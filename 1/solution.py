40668f05fc6c9ebe7eafc4e2817ac04256871163
DemetreJou <demetre.jouras@gmail.com>
2018-12-05 23:30:31 -0500

    Solved day 1 part 2

--- 1/solution.py
+++ 1/solution.py
@@ -4,12 +4,33 @@ reading from file
 """
 
 # part 1
+# simply read the file
 
 filename = 'input.txt'
-result = 0
-with open(filename, 'r') as f:
-    for line in f:
-        result += int(line)
-    print(result)
+
+
+def findSum():
+    result = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            result += int(line)
+        return (result)
 
 # part 2
+# keep track of what's been read
+
+import itertools
+
+
+def findrepeat():
+    data = [int(x) for x in open(filename, 'r').readlines()]
+    result = 0
+    seen = {0}  # optimized for searching through
+    for num in itertools.cycle(data):
+        result += num
+        if result in seen:
+            return (result)
+        seen.add(result)
+
+
+print(findrepeat())
